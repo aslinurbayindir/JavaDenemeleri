2881f87660c5963e41eefcad68a32fb080673814
aslinurb <aslinurbayindir@gmail.com>
2019-03-29 16:04:36 +0300

    Create MerhabaDunya.java

---
+++ MerhabaDunya.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Asli
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int deg;
+        System.out.println("Merhaba Dunya");
+        System.out.println("ASlınur BAyındır");
+        
+        deg=10;
+        System.out.println("ASlınur BAyındır "+ deg);
+        
+        String s = "benim adım Aslınur BAYINDIR";
+        System.out.println("dizgi " + s);
+        
+        double pi = 3.14;
+        System.out.println("pi : " + pi);
+        
+        
+    }
+    
+}
