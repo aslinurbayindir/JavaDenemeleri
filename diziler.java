8f6509075d165f5df95e6d9c6015abc0716b0fe9
aslinurb <aslinurbayindir@gmail.com>
2019-04-02 11:16:04 +0300

    Create diziler.java

---
+++ diziler.java
@@ -0,0 +1,24 @@
+
+package javaapplication2;
+
+/**
+ *
+ * @author Asli
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a [] = new int [5];
+        a[3] = 8;
+        System.out.println(""+a[3]);
+        
+        String str[] = {"ali","baba","ve","kirk","haramiler"};
+        System.out.println(str[2]+str[4]);
+        
+    }
+    
+}
